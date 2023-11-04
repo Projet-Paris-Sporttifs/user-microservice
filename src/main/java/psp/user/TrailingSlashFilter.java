05945f7ce5ca89831f669d4c45e0c787504a0fe6
Ibrahima Sylla <isyll711@gmail.com>
2023-11-04 21:29:33 +0000

    fix :ambulance: redirect trailing slashes

---
+++ src/main/java/psp/user/TrailingSlashFilter.java
@@ -0,0 +1,37 @@
+package psp.user;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class TrailingSlashFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        if (request.getRequestURI().endsWith("/")) {
+            ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromRequest(request);
+            String path = builder.build().getPath();
+            builder.replacePath(String.format("%s", path.substring(0, path.length() - 1)));
+            response.setStatus(HttpStatus.MOVED_PERMANENTLY.value());
+            response.setHeader(HttpHeaders.LOCATION, builder.toUriString());
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
